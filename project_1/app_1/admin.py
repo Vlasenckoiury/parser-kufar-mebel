f97f42a38aa2d77d046f7ca7ac90179fe7b1e0de
vlase <vlasencko_iury1999@yandex.by>
2023-09-07 23:56:53 +0300

    Добавление больше функций в админку

--- project_1/app_1/admin.py
+++ project_1/app_1/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Mebel
 
-admin.site.register(Mebel)
+
+class MebelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'price', 'description', 'parse_datetime']  # заголовки
+    list_display_links = ['id', 'description']  # переход в карточку
+    search_fields = ['id', 'price', 'description', 'parse_datetime']  # добавляет поиск([] по каким параметрам искать)
+    # list_editable = ['price'] # позволяет в админке менять цену, не заходя в карточку
+    list_filter = ['parse_datetime', 'price']
+
+
+admin.site.register(Mebel, MebelAdmin)
